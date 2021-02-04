205794a74d6629c9a23f3a92463f347710c1809e
AlexPuenteJara <alex.puente1@unmsm.edu.pe>
2021-02-03 23:27:23 -0500

    Holibb

---
+++ F. Nezzar and Nice Beatmap.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define debug(x) cout << #x << " = " << x << endl
+#define fastio ios_base::sync_with_stdio(false);cin.tie(NULL)
+typedef long long ll;
+typedef pair<int, int> ii;
+typedef vector<int> vi;
+typedef vector<ii> vii;
+const int MAXN=1e5;
+const int inf=1e9;
+vector<int> g[MAXN] ;
+bool used[MAXN] ;
+vector<int> comp ;
+int main(){
+ fastio;
+ int t;
+ cin>>t;
+ while(t--){
+	 int n;
+	 cin>>n;
+     cout<<"\n";
+    }
+   return 0;
+}
+ 
